2d60ba999fc2c04b0d12109e243134a552b604fb
Arber Baba <arberi.hh@gmail.com>
2024-03-07 22:26:40 +0100

    main: toggle theme
    Signed-off-by: Arber Baba <arberi.hh@gmail.com>

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
+import { Pressable } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Home, User } from '../screens';
 import { useTheme } from '../context';
+
 
 const Stack = createNativeStackNavigator();
 
 function Navigation() {
-  const { theme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <Stack.Navigator screenOptions={{
       headerStyle: {
@@ -16,7 +19,11 @@ function Navigation() {
       headerTitleStyle: {
         fontWeight: 'bold',
       },
-
+      headerRight: () => (
+        <Pressable onPress={toggleTheme}>
+          <MaterialIcons name="dark-mode" size={24} color={theme.color} />
+        </Pressable>
+      ),
     }}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="User" component={User} />
